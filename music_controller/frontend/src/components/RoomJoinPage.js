c19740f7f9620487a4e734430730de38ab527aa2
Apoorv Garg <u.garg.10@gmail.com>
2021-09-10 13:43:08 +0530

    Backend

---
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class RoomJoinPage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <p>This is Room Join Pages</p>;
+    }
+}
