b4c28fab5abc58d543e1a9c76e343591ce7c84f2
FalseG0d <garg.apoorv1098@gmail.com>
2022-04-15 13:53:04 +0530

    Lect 9

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from rest_framework import generics, serializers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 from .models import Room
 from  .serializers import RoomSerializer, CreateRoomSerializer
@@ -93,3 +94,15 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
             return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data={
+            'code':self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import {Grid, Button, ButtonGroup, Typography} from '@material-ui/core';
 
 import {
     BrowserRouter as Router,
@@ -14,6 +15,37 @@ import {
 export default class HomePage extends Component{
     constructor(props){
         super(props);
+        this.setState = {
+            roomCode : null
+        };
+    }
+
+    async componentDidMount(){
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code
+            })
+        })
+    }
+
+    renderHomePage(){
+        return (
+            <Grid container spacing={3}>
+            <Grid item xs = {12} align="center">
+                <Typography variant='h3' compact='h3'>
+                    Silent DJ
+                </Typography>
+            </Grid>
+            <Grid item xs = {12} align="center">
+                <ButtonGroup disableElevation variant='contained' color='primary'>
+                    <Button color="primary" to="/join" component={Link}>Join A Room</Button>
+                    <Button color="secondary" to="/create" component={Link}>Create A Room</Button>
+                </ButtonGroup>
+            </Grid>
+            </Grid>
+        );
     }
 
     render(){
@@ -21,7 +53,7 @@ export default class HomePage extends Component{
             <Router>
                 <Switch>
                     <Route exact path="/">
-                        <p>This is the Home Page</p>
+                        {this.renderHomePage()}
                     </Route>
                     <Route path="/join" component={RoomJoinPage}/>
                     <Route path="/create" component={CreateRoomPage}/>
